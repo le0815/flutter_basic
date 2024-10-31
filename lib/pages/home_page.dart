3862897145cbb960726ad1b089f1baa95be83680
ha <hnngcs2@gmail.com>
2024-10-31 11:46:26 +0700

    cloned todoapp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_3/database/local_database.dart';
-import 'package:flutter_application_3/util/add_todo_dialog.dart';
-import 'package:flutter_application_3/util/todo_tile.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_application_3/utils/dialog_create_new.dart';
+import 'package:flutter_application_3/utils/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,73 +11,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final _controller = TextEditingController();
-  LocalDataBase db = LocalDataBase();
+  TextEditingController controller = TextEditingController();
+  LocalDatabase db = LocalDatabase();
 
   @override
   void initState() {
     // TODO: implement initState
-    db.LoadData();
+    db.LoadDatabase();
     super.initState();
   }
 
-  void CheckBoxChange(bool? value, int index) {
+  void ChangeState(bool? value, int index) {
     setState(() {
       db.toDoList[index][1] = !db.toDoList[index][1];
     });
-    db.UpdateData();
+    db.UpdateDatabase();
   }
 
-  void CreateNewTask() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AddTodoDialog(
-          controller: _controller,
-          onBack: () => Navigator.of(context).pop(),
-          onSave: SaveTask,
-        );
-      },
-    );
-  }
-
-  void SaveTask() {
-    setState(() {
-      db.toDoList.add([_controller.text, false]);
-      _controller.clear();
-    });
-    //back to homescreen
+  void SaveTodo() {
+    String text = controller.text;
+    if (text.isNotEmpty) {
+      setState(() {
+        db.toDoList.add([text, false]);
+      });
+    }
+    db.UpdateDatabase();
+    controller.clear();
     Navigator.of(context).pop();
-    db.UpdateData();
   }
 
-  void RemoveTask(int index) {
+  void RemoveTodo(int index) {
     setState(() {
       db.toDoList.removeAt(index);
     });
-    db.UpdateData();
+    db.UpdateDatabase();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[200],
       appBar: AppBar(
-        title: Text("My Todo"),
+        title: const Text("Todo App"),
         backgroundColor: Colors.blue[400],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: CreateNewTask,
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return DialogCreateNew(
+                  controller: controller,
+                  onBack: () => Navigator.of(context).pop(),
+                  onSave: SaveTodo,
+                );
+              });
+        },
         child: Icon(Icons.add),
       ),
       body: ListView.builder(
         itemCount: db.toDoList.length,
         itemBuilder: (context, index) {
           return TodoTile(
-            name: db.toDoList[index][0],
-            completed: db.toDoList[index][1],
-            onChanged: (value) => CheckBoxChange(value, index),
-            onRemove: () => RemoveTask(index),
+            checkState: db.toDoList[index][1],
+            text: db.toDoList[index][0],
+            onChanged: (value) => ChangeState(value, index),
+            onRemove: () => RemoveTodo(index),
           );
         },
       ),
