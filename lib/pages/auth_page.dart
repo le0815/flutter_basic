749537487dd7c839ee8810742987b4ad65022a53
ha <hnngcs2@gmail.com>
2024-11-08 09:09:46 +0700

    finish authentication with firebase

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_5/pages/home_page.dart';
 import 'package:flutter_application_5/pages/login_or_register_page.dart';
-import 'package:flutter_application_5/pages/login_page.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
